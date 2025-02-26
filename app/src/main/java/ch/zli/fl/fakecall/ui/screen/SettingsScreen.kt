10b6b959d404aed2dada8d12067c6a3bd77b332e
fliberto <fabio.liberto@ergon.ch>
2025-02-26 14:32:46 +0100

    Add unversioned files - Add SettingsScreen with title

---
+++ app/src/main/java/ch/zli/fl/fakecall/ui/screen/SettingsScreen.kt
@@ -0,0 +1,26 @@
+package ch.zli.fl.fakecall.ui.screen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "FakeCall",
+                        fontSize = 40.sp,
+                    )
+                },
+            )
+        },
+    ) { _ ->
+    }
+}
